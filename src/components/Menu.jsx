50c67d35fa192648a494ff92a724a5e0534ee423
Steven Pierre <96037516+kiss42@users.noreply.github.com>
2025-06-06 22:04:21 -0400

    update

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import menuItems from '../data/menu';
 import FoodCard from './FoodCard';
+import menuBackground from '../assets/menubackground.png';
 
 const Menu = () => {
   return (
     <section
       id="menu"
-      className="bg-gradient-to-b from-[#00209F] via-[#eaeaea] to-white py-20 px-6"
+      className="relative bg-cover bg-center py-20 px-6"
+      style={{
+        backgroundImage: ` url(${menuBackground})`,
+      }}
     >
       <div className="max-w-7xl mx-auto text-center">
         <h2 className="text-4xl font-extrabold text-white drop-shadow-md mb-10 tracking-wide">
